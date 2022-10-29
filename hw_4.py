44f056f1e998d802dfad8359a2b19cc161cdc69d
Алина Прокофьева <ve4nayavesna@mail.ru>
2022-10-30 02:11:12 +0300

    init 2

--- hw_4.py
+++ hw_4.py
@@ -9,6 +9,7 @@ nested_list = [
 
 '''итератор, который принимает список списков, и возвращает их плоское представление'''
 
+# вариант с "разглаживанием" списка до итерации
 class FlatIterator:
     def __init__(self, list_):
         self.list_ = sum(list_, [])
@@ -24,6 +25,42 @@ class FlatIterator:
         item = self.list_[self.cursor]
         self.cursor += 1
         return item
+
+# проверяем
+for item in FlatIterator(nested_list):
+    print(item)
+print('=' * 80)
+
+# вариант с "разглаживанием" в процессе итерации
+class FlatIterator:
+    def __init__(self, list_):
+        self.list_ = list_
+        self.len = len(sum(list_, []))
+
+    def __iter__(self):
+        self.cursor = 0
+        self.iter_cur = 0
+        self.iter_item = self.list_[self.iter_cur]
+        self.inner_cur = -1
+        return self
+
+    def __next__(self):
+        self.inner_cur += 1
+
+        if self.cursor >= self.len:
+            raise StopIteration
+
+        elif self.inner_cur == len(self.iter_item):
+            self.iter_cur += 1
+            self.inner_cur = -1
+            self.iter_item = self.list_[self.iter_cur]
+            self.inner_cur += 1
+            self.cursor += 1
+            return self.iter_item[self.inner_cur]
+
+        elif self.inner_cur < len(self.iter_item):
+            self.cursor += 1
+            return self.iter_item[self.inner_cur]
 
 # проверяем
 for item in FlatIterator(nested_list):
